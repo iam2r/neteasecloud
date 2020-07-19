05f81f8277e7bc0ee64c339e5a083dcc44452d2b
zr <bestwishtous@gmail.com>
2020-07-20 02:14:02 +0800

    自动注入scss全局变量和mixins

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-    transpileDependencies: [/swiper/, /dom7/]
+    publicPath: '',
+    devServer: {
+        inline: true,
+        hot: true, 
+        open: true,
+        host: '0.0.0.0',
+        useLocalIp: true,
+        historyApiFallback: true,
+    },
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `
+                @import "~@/styles/_var.scss";
+                @import "~@/styles/_mixins.scss";
+                `
+            }
+        }
+    },
+    transpileDependencies: [/swiper/, /dom7/, /strip-ansi/], //IE11兼容，正则匹配强制走babel转换
 }
