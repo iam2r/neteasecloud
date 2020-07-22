8b01cd69d37e8107377dc10fbff9a82df67e4cb9
zr <bestwishtous@gmail.com>
2020-07-23 03:37:04 +0800

    test

--- src/state.ts
+++ src/state.ts
@@ -1,22 +1,20 @@
 import Vue from "vue";
 
 declare module "vue/types/vue" {
-    interface Vue {
-        $state: State;
-        _uid: number;
-    }
+  interface Vue {
+    $state: State;
+    _uid: number;
+  }
 }
-
 
 export interface State {
-    transitions: { [key: string]: string, pages: string }
+  transitions: { [key: string]: string; pages: string };
 }
 
-
 export const state: State = {
-    transitions: {
-        pages: 'fade'
-    }
+  transitions: {
+    pages: "fade",
+  },
 };
 
 export default Vue.prototype.$state = Vue.observable(state);
