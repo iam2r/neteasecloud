cc0776b0aef94d2eb68702ea5a12eb6a07275c63
zr <bestwishtous@gmail.com>
2020-07-26 03:32:52 +0800

    222

--- src/views/Account/index.tsx
+++ src/views/Account/index.tsx
@@ -1,6 +1,6 @@
 import { Component as tsx } from "vue-tsx-support";
 import { Component } from "vue-property-decorator";
-
+import "./style.scss";
 @Component
 export default class Account extends tsx<any> {
   protected render() {
